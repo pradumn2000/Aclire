e64626414b3988929b91917cd1d3e55a43790998
pradumn2000 <ayush.gup555@gmail.com>
2026-05-07 00:25:47 +0530

    login done.

--- resources/js/pages/Login.jsx
+++ resources/js/pages/Login.jsx
@@ -209,13 +209,8 @@ const handleSubmit = async (e) => {
 <div className="modal fade" id="sucessModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog modal-dialog-centered">
     <div className="modal-content all-pop-bg">
-      {/* <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div> */}
       <div className="modal-body">
         <div className="login-succ-in">
-            {/* <img src="images/happy-icon.png" alt="Happy Icon"> */}
             <h3>Login Successful</h3>
             <p>Welcome to <span>Styapan</span></p>
             <form>
