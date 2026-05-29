bfb67560dbcf45bd96ee063d7f04afd629852581
pradumn2000 <ayush.gup555@gmail.com>
2026-05-30 02:59:35 +0530

    fix: forgot-password endpoint typo

--- resources/js/pages/Forgetpassword.jsx
+++ resources/js/pages/Forgetpassword.jsx
@@ -94,7 +94,7 @@ export default function ForgotPassword() {
 
     try {
       // const res = await fetch("http://localhost:8000/api/forgot-password",
-      const res = await fetch(`${API_URL}/api/forget-password`,  {
+      const res = await fetch(`${API_URL}/api/forgot-password`,  {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
