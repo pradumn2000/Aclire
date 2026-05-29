8207b325e69a8d552939128f4134c7d074638f67
pradumn2000 <ayush.gup555@gmail.com>
2026-05-29 23:28:12 +0530

    Fix HTTPS proxy handling

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+    'http://localhost:3000',
+    'https://authbridge-10.onrender.com',
+],
 
     'allowed_origins_patterns' => [],
 
