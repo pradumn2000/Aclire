9b2a90842ebf35a17f7dae4d92c75c2deb04036c
pradumn2000 <ayush.gup555@gmail.com>
2026-06-09 00:16:53 +0530

    fix invalid useCases hook call

--- resources/js/pages/AllCases.jsx
+++ resources/js/pages/AllCases.jsx
@@ -227,9 +227,9 @@
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { useCases } from "../hooks/useCases";
-import { useCaseFilters } from "../hooks/useCaseFilters";
-import DateRangePicker from "../components/DateRangePicker";
+import { useCases } from "../src/hooks/useCases";
+import { useCaseFilters } from "../src/hooks/useCaseFilters";
+import DateRangePicker from "../src/components/DateRangePicker";
 
 const STATUS_TABS   = ["All", "In Progress", "QC Review", "Pending", "Completed"];
 const STATUS_MAP    = {
